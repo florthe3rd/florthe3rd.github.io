162a70957f87c31a038ad397af16bf018df267b5
florante <49671861+florthe3rd@users.noreply.github.com>
2019-04-17 16:56:49 +0800

    photo

---
+++ js/script.js
@@ -0,0 +1,48 @@
+
+
+	
+	$(document).ready(function(){
+
+
+//nav toggle 
+		$('.toggle').click(function(){
+			$('.display').toggleClass('active');
+		});
+
+		$('nav ul li a').on("click", function(){
+					$('.display').removeClass('active');
+			});
+
+
+
+
+//scroll lock
+		$('.scroll').click(function(){
+				$('body').css({
+					"overflow":"hidden"
+				});
+		});
+
+
+
+
+
+
+//scroll unlock
+		//	
+		$('.lb-close').click(function(){
+			$('body').css({
+					"overflow":"auto"
+				});
+		});
+		$('.lightboxOverlay').click(function(){
+			$('body').css({
+					"overflow":"auto"
+				});
+		});
+
+		
+
+});
+
+	
