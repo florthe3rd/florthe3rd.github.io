7fdbf5301d3e6a6108693e4a99bc376aedeeac40
florante <49671861+florthe3rd@users.noreply.github.com>
2019-05-03 18:43:43 +0800

    toggle

--- photograper/js/script.js
+++ photograper/js/script.js
@@ -7,10 +7,12 @@
 //nav toggle 
 		$('.toggle').click(function(){
 			$('.display').toggleClass('active');
+			$('.toggle').toggleClass('active');
 		});
 
 		$('nav ul li a').on("click", function(){
 					$('.display').removeClass('active');
+					$('.toggle').removeClass('active');
 			});
 
 
